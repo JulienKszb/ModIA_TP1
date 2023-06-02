de90dad53be72922c9e0c9de1afe83ca146303c5
Julien Kaszuba <julienkaszuba@MacBook-Air-de-Julien.local>
2023-06-02 15:03:46 +0200

    commit to train on cloud instance

--- colorize_app.py
+++ colorize_app.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from unet import UNet
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('mps')
 source_process = transforms.Compose(
         [transforms.Resize((224, 224)), transforms.ToTensor(),
          transforms.Normalize(mean=[0.5], std=[0.5])])
@@ -18,9 +18,9 @@ def recognize_digit(image):
     return "colorized.png"
 
 if __name__=='__main__':
-    parser = ...
+    parser = argparse.ArgumentParser()
     parser.add_argument(...)
-    args = ...
+    args = parser.parse_args()
 
     model = UNet().to(device)
     model.load_state_dict(..., map_location=torch.device(device))
